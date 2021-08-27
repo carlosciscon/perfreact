3192ff8ea19bb52c4151621a5c4f75144face50b
carlosciscon <carlosciscon@gmail.com>
2021-08-27 15:20:02 -0300

    implementaçoes

---
+++ components/ProductItem.tsx
@@ -0,0 +1,34 @@
+import { memo, useState } from 'react';
+import { AddProductToWishlist } from './AddProductToWhislist';
+
+interface ProductItemProps {
+    product: {
+        id: number;
+        price: number;
+        priceFormatted : string;
+        title: string;
+    }
+    onAddToWishlist: (id: number) => void;
+}
+
+export function ProductItemComponent({product, onAddToWishlist}: ProductItemProps) {
+    const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
+    return (
+        <div>
+            {product.title} - <strong>{product.priceFormatted}</strong>
+            <button onClick={() => setIsAddingToWishlist}>Adicionar aos favoritos</button>
+
+            {isAddingToWishlist && (
+                <AddProductToWishlist 
+                    onAddToWishlist={() => onAddToWishlist(product.id)}
+                    onRequestClose={() => setIsAddingToWishlist(false)}
+                />
+            )}
+        </div>    
+    )
+}
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product)
+})
